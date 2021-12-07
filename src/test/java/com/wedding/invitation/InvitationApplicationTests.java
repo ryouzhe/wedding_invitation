ceafff2756b4831929d9d5d3305a0ff9e79f41fc
ryouzhe <sjh3183@naver.com>
2021-12-07 15:13:54 +0900

    message

---
+++ src/test/java/com/wedding/invitation/InvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedding.invitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
